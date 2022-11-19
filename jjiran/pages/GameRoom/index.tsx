892109af8f6f0eb5386cf20de5eef5d081996249
Kang Jisoon <wltnskjs@naver.com>
2022-11-19 17:01:13 +0900

    🌱 GameRoom 페이지 파일 추가

---
+++ jjiran/pages/GameRoom/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+const GameRoom = () => {
+	return(
+		<div>
+			<Container maxWidth="lg">
+				<h1>GAME ROOM</h1>
+			</Container>
+		</div>
+	);
+};
+
+export default GameRoom;
