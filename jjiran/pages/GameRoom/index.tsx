fa30bd2eba1d152871f6f04d699a45a017de4c2f
Kang Jisoon <wltnskjs@naver.com>
2022-11-22 14:16:30 +0900

    ♻️ 컴포넌트 분리

--- jjiran/pages/GameRoom/index.tsx
+++ jjiran/pages/GameRoom/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Container, Stack, Button, IconButton, Divider, Tooltip } from "@mui/material";
+import { Container, Stack, Button, IconButton, Divider, Tooltip, Box } from "@mui/material";
 import CancelIcon from '@mui/icons-material/Cancel';
 import gravatar from 'gravatar';
+import PrintHostVsPlayer from "@components/PrintHostVsPlayer";
 
 const GameRoom = () => {
 	return(
@@ -22,17 +23,7 @@ const GameRoom = () => {
 					</Tooltip>
 				</Stack>
 				<Divider variant="middle" />
-				<Stack direction="row" justifyContent="space-between">
-					<div>
-						<img src={gravatar.url('wltnskjs@naver.com', { s: '36px', d: 'retro' })} />
-						<b>jisokang</b>
-					</div>
-					<h3> VS </h3>
-					<div>
-						<b>jisokang2</b>
-						<img src={gravatar.url('wltnskjs@naver.com', { s: '36px', d: 'retro' })} />
-					</div>
-				</Stack>
+				<PrintHostVsPlayer />
 				{/* nickname 불러와서 출력해줘야함 */}
 				<h2>observer list</h2>
 				<div>hyopark</div>
