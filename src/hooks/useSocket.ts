e31fc2d5712e276be38bef3004632e3cfd1c5c42
zxcv3508 <37266744+zxcv3508@users.noreply.github.com>
2022-12-08 19:17:01 +0900

    Update useSocket.ts

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -2,9 +2,15 @@ import { useCallback } from 'react';
 import { io, Socket } from 'socket.io-client'
 
 const backUrl = 'https://server.gilee.click/api/socket';
+import useSWR from 'swr';
+import authfetcher from 'src/utils/authfetcher';
 
 const sockets: { [key: string]: Socket } = {};
 const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
+  const {data, mutate} = useSWR('token', authfetcher ,{
+    dedupingInterval:100000
+  });
+  
   const disconnect = useCallback(() => {
     if (workspace && sockets[workspace]) {
       sockets[workspace].disconnect();
@@ -15,11 +21,15 @@ const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
     return [undefined, disconnect];
   }
   if (!sockets[workspace]) {
+    console.log("auth token:", 'Bearer ' + data);
     sockets[workspace] = io(`${backUrl}`, {
       transports: ['websocket'],
-//       extraHeaders: {
-//      "Sec-WebSocket-Protocol": "chat, superchat"
-//   }
+      extraHeaders: {
+        authorization: 'Bearer ' + data,
+        // "Sec-WebSocket-Protocol": "chat",
+    //     "test": "chat",
+    //     "tetet": "chat",
+     }
     });
     console.info('create socket', workspace, sockets[workspace]);
   }
@@ -27,4 +37,4 @@ const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
   return [sockets[workspace], disconnect];
 };
 
-export default useSocket;
+export default useSocket;`
