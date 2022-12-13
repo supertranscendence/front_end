fada2fa9c3e4f842b21f1e80cf1c56d4104a57a4
zxcv3508 <37266744+zxcv3508@users.noreply.github.com>
2022-12-13 10:45:00 +0900

    Update useSocket.ts

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,8 +1,8 @@
 import { useCallback } from 'react';
 import { io, Socket } from 'socket.io-client'
 
-const backUrl = 'https://server.gilee.click/api/socket';
-//const backUrl = 'http://127.0.0.1:3000/api/socket';
+//const backUrl = 'https://server.gilee.click/api/socket';
+const backUrl = 'http://127.0.0.1:3000/api/socket';
 
 import useSWR from 'swr';
 import authfetcher from 'src/utils/authfetcher';
@@ -23,15 +23,15 @@ const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
     return [undefined, disconnect];
   }
   if (!sockets[workspace]) {
-    console.log("auth token:", 'Bearer ' + data);
+    console.log("auth token:", 'Bearer ' + localStorage.getItem("accessToken"));
     sockets[workspace] = io(`${backUrl}`, {
       transports: ['websocket'],
       auth: {
-        token:'Bearer '+ data,
-        authorization:'Bearer '+ data
+        token:'Bearer '+ localStorage.getItem("accessToken"),
+        authorization:'Bearer '+ localStorage.getItem("accessToken")
       },
       extraHeaders: {
-        authorization: 'Bearer ' + data,
+        authorization: 'Bearer ' + localStorage.getItem("accessToken"),
         // "Sec-WebSocket-Protocol": "chat",
     //     "test": "chat",
     //     "tetet": "chat",
