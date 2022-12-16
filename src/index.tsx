a6bb9e25f701c5515a164be1bd502a198edaa124
Kang Jisoon <wltnskjs@naver.com>
2022-12-16 18:47:59 +0900

    💄 Update UI like Slack default

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-
 import App from './layouts/App/App';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from 'src/theme';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL =
@@ -13,7 +14,9 @@ axios.defaults.baseURL =
 
 render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.querySelector('#app'),
 );
