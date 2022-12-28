70e6e703a0f938d859962db6d5ca1b9bc881a684
Kang Jisoon <wltnskjs@naver.com>
2022-12-28 16:29:15 +0900

    Fix first login

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -47,12 +47,12 @@ const Intro = () => {
     .then(() => {
       console.log("userAchi: ", userAchi);
       if(userAchi && userAchi[0].achievement === 0) {
-        console.log("NOT 처음 로그인!");
-        setIsFirstLogin(false);
-      }
-      else{
         console.log("😄 첫번째 로그인");
         setIsFirstLogin(true);
+      }
+      else{
+        console.log("NOT 처음 로그인!");
+        setIsFirstLogin(false);
       }
     })
     .catch((err) => {
